4f4b93d4c77382d19628e4a0b180e763de04e762
Brandon Simmons <bsimmons1@ycp.edu>
2022-03-07 13:45:51 -0500

    added methods for hardcoded credentials

--- CS320_YCP_Personalized_Commencement_Project/src/edu/ycp/cs320/personalized_commencement/controller/StudentController.java
+++ CS320_YCP_Personalized_Commencement_Project/src/edu/ycp/cs320/personalized_commencement/controller/StudentController.java
@@ -1,8 +1,13 @@
 package edu.ycp.cs320.personalized_commencement.controller;
 
+import edu.ycp.cs320_personalized_commencement.model.Student;
+
 /**
  * Acts as the controller for the Student model class.
  */
 public class StudentController extends UserController{
 
+	public void loginStudent(Student student) {
+		
+	}
 }
--- CS320_YCP_Personalized_Commencement_Project/src/edu/ycp/cs320_personalized_commencement/model/Student.java
+++ CS320_YCP_Personalized_Commencement_Project/src/edu/ycp/cs320_personalized_commencement/model/Student.java
@@ -5,7 +5,15 @@ package edu.ycp.cs320_personalized_commencement.model;
  */
 public class Student extends User{
 	// field Object for the student's info
-	private Object studentInfo;
+	private Object studentInfo; 
+	private String username, email, password; // Hardcoded elements
+	
+	public void setCredentials(String username, String email, String password) {
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
+	
 	
 	/**
 	 * Sets the studentInfo object with the info they submitted.
