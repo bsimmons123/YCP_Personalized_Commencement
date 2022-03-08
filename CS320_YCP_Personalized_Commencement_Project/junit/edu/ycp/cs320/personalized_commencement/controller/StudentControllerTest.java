f207bd5543730d246176bdc30bb3c3de4db18385
erosenberry <98916411+erosenberry@users.noreply.github.com>
2022-03-08 14:14:51 -0500

    Added test cases for student controller, might be something wrong with student controller checkStudentLogin()

--- CS320_YCP_Personalized_Commencement_Project/junit/edu/ycp/cs320/personalized_commencement/controller/StudentControllerTest.java
+++ CS320_YCP_Personalized_Commencement_Project/junit/edu/ycp/cs320/personalized_commencement/controller/StudentControllerTest.java
@@ -1,5 +1,50 @@
 package edu.ycp.cs320.personalized_commencement.controller;
 
-public class StudentControllerTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.ycp.cs320_personalized_commencement.model.StudentModel;
+import edu.ycp.cs320_personalized_commencement.model.UserModel;
+
+public class StudentControllerTest {
+	private StudentModel model;
+	private UserModel user;
+	private StudentController controller;
+		
+		@Before
+		public void setup() {
+			//set up model
+			model = new StudentModel();
+			model.setEmail("EthanLicksWindows@hotmail.yahoo.brown");
+			model.setPassword("Brandon'sMom1");
+			model.setStudent(true);
+			
+			
+			//set up user
+			user = new UserModel();
+			
+			// set up controller
+			controller = new StudentController();
+			controller.setModel(model);
+			
+		}
+		@Test
+		public void testCheckStudentLogin(){
+			user.setEmail("EthanLicksWindows@hotmail.yahoo.brown");
+			user.setPassword("Brandon'sMom1");
+			user.setStudent(true);
+			model.setStudent(true);
+			assertEquals(true, controller.checkStudentLogin(user));
+			
+			user.setEmail("AndrewLicksLinix@hotmail.com");
+			user.setPassword("Rob'sMom2");
+			user.setStudent(false);
+			model.setStudent(false);
+			assertEquals(false, controller.checkStudentLogin(user));
+			
+		}
+		
+		
 }
