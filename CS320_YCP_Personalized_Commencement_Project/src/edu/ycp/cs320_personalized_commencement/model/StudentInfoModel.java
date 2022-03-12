9aed9238610f6abc4c239fbb69f8a1c133c062e5
erosenberry <98916411+erosenberry@users.noreply.github.com>
2022-03-12 01:17:22 -0500

    Added Student Info Model with getters and setters

---
+++ CS320_YCP_Personalized_Commencement_Project/src/edu/ycp/cs320_personalized_commencement/model/StudentInfoModel.java
@@ -0,0 +1,64 @@
+package edu.ycp.cs320_personalized_commencement.model;
+
+public class StudentInfoModel {
+	private String firstName;
+	private String middleInitial;
+	private String lastName;
+	private String major;
+	private String minor;
+	private String extraCur;
+	private Object picture;
+	private Object nameSound;
+	
+	//setters
+	public void setFirstName(String firstName){
+		this.firstName = firstName;
+	}
+	public void setMiddleInitial(String middleInitial) {
+		this.middleInitial = middleInitial;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	public void setMinor(String minor) {
+		this.minor = minor;
+	}
+	public void setExtraCur(String extraCur) {
+		this.extraCur = extraCur;
+	}
+	public void setPicture(Object picture) {
+		this.picture = picture;
+	}
+	public void setNameSound(Object nameSound) {
+		this.nameSound = nameSound;
+	}
+	
+	//getters
+	public String getFirstName(){
+		return firstName;
+	}
+	public String getMiddleInital() {
+		return middleInitial;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public String getMajor() {
+		return major;
+	}
+	public String getMinor() {
+		return minor;
+	}
+	public String getExtraCur() {
+		return extraCur;
+	}
+	public Object getPicture() {
+		return picture;
+	}
+	public Object getNameSound() {
+		return nameSound;
+	}
+}
